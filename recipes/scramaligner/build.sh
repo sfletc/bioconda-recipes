ecbf09a4a630c0a6212931d23b03727387fbe770
sfletc <steveofbrisbane@gmail.com>
2023-04-21 12:21:43 +1000

    fix

--- recipes/scramaligner/build.sh
+++ recipes/scramaligner/build.sh
@@ -9,12 +9,12 @@ go build -o "${PREFIX}/bin/scramAligner" .
 # Install Python scripts
 # Install adaptTrim
 pushd scramScripts/adaptTrim/src
-${PYTHON} -m pip3 install . --no-deps --ignore-installed --no-cache-dir -vvv
+${PYTHON} -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 popd
 
 # Install scram2Plot
 pushd scramScripts/scram2Plot/src
-${PYTHON} -m pip3 install . --no-deps --ignore-installed --no-cache-dir -vvv
+${PYTHON} -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 popd
 
 # Make Python scripts executable and move them to the bin folder
