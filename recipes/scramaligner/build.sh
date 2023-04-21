db89410af83cf373c25491703e2bcb64f1f558b6
sfletc <steveofbrisbane@gmail.com>
2023-04-21 11:38:16 +1000

    testing

---
+++ recipes/scramaligner/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# Build Go app
+export GOPATH="${SRC_DIR}/gopath"
+export PATH="$GOPATH/bin:$PATH"
+go build -o "${PREFIX}/bin/scramAligner" .
+
+# Install Python scripts
+# Install adaptTrim
+pushd scramScripts/adaptTrim/src
+${PYTHON} -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+popd
+
+# Install scram2Plot
+pushd scramScripts/scram2Plot/src
+${PYTHON} -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+popd
+
+# Make Python scripts executable and move them to the bin folder
+find scramScripts -type f -name "*.py" -exec chmod +x {} \;
+find scramScripts -type f -name "*.py" -exec cp {} "${PREFIX}/bin/" \;
